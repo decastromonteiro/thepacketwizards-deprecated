804679b8782d017feec802590844cc8282b9858a
Leonardo Monteiro <decastromonteiro@gmail.com>
2020-02-27 20:49:20 -0300

    fixed featured non published posts showing up on landing page

--- thepacketwizards/views.py
+++ thepacketwizards/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from blog.models import BlogPost
-
+from django.utils import timezone
 
 def home_page(request):
     title = "The Packet Wizards"
+    now = timezone.now()
     qs = BlogPost.objects.all().order_by('-publish_date').filter(
         published=True,
-        featured=True
+        featured=True,
+        publish_date__lte=now
     )[:3]
     context = {"title": title, 'blog_list': qs}
     return render(request, "index/index.html", context)
